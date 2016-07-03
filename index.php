92c3b2df434d5f8a6ab4461d5e4a4d4b7941908f
makoto abe <abe00makoto@gmail.com>
2016-07-03 13:47:57 +0900

    php7
    php7対応

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $link = mysql_connect('localhost', 'root'); ?>
+<?php $link = mysqli_connect('localhost', 'root'); ?>
 <html>
 <head>
 	<title>Hello world!</title>
@@ -21,7 +21,7 @@
 	<?php if(!$link) { ?>
 		<h2>Can't connect to local MySQL Server!</h2>
 	<?php } else { ?>
-		<h2>MySQL Server version: <?php echo mysql_get_server_info(); ?></h2>
+		<h2>MySQL Server version: <?php echo mysqli_get_server_info($link); ?></h2>
 	<?php } ?>
 </body>
 </html>
